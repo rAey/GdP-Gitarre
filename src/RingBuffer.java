b8ba1971899e8a3796a293922397ded83369d22d
Dustin Fechner <dfe@hush.com>
2013-01-25 16:10:36 +0100

    Add: Implementation of RingBuffer
    Works OK for now, but should be polished later.

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,38 +1,68 @@
 public class RingBuffer {
 
+private double buffer[];
+private int first, last, size;
+
 // constructor to create an empty buffer, with given max capacity
 RingBuffer(int capacity) {
-
+	buffer = new double[capacity];
+	first = last = size = 0;
 }
 
 // return number of items currently in the buffer
 int size() {
-	return 0;
+	return size;
 }
 
 // is the buffer empty (size equals zero)?
 boolean isEmpty() {
+	if (size <= 0)
+		return true;
 	return false;
 }
 
 // is the buffer full  (size equals capacity)?
 boolean isFull() {
+	if (size >= buffer.length)
+		return true;
 	return false;
 }
 
 // add item x to the end
 void enqueue(double x) {
-
+	if (isFull()) {
+		// TODO: Better throw exception here...
+		return;
+	}
+	
+	buffer[last] = x;
+	
+	last++;
+	size++;
+	if (last >= buffer.length)
+		last = 0;
 }
 
 // delete and return item from the front
 double dequeue() {
-	return 0;
+	if (isEmpty()) {
+		// TODO: Better throw exception here...
+		return 0.;
+	}
+	
+	double x = buffer[first];
+	
+	first++;
+	size--;
+	if (first >= buffer.length)
+		first = 0;
+	
+	return x;
 }
 
 // return (but do not delete) item from the front
 double peek() {
-	return 0;
+	return buffer[first];
 }
 
 }
