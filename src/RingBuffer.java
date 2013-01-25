483c35e0afe191f1f11dc9335b4a4e21188487e8
Dustin Fechner <dfe@hush.com>
2013-01-25 15:26:39 +0100

    Fix: Whitespace errors

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -1,50 +1,38 @@
-/**
- * 
- * @author Robert
- *
- */
 public class RingBuffer {
-	
-    RingBuffer(int capacity){
-    	// constructor to create an empty buffer, with given max capacity
-    }
-    
-   	// return number of items currently in the buffer
-    int size(){
-		return 0;
- 
-    }
-    
-	// is the buffer empty (size equals zero)?
-    boolean isEmpty(){
-		return false;
 
-    }
-    
-	// is the buffer full  (size equals capacity)?
-    boolean isFull(){
-		return false;
+// constructor to create an empty buffer, with given max capacity
+RingBuffer(int capacity) {
 
-    }
-    
-    // add item x to the end
-   void enqueue(double x){
+}
 
-   }
-   
-   // delete and return item from the front
-   double dequeue(){
+// return number of items currently in the buffer
+int size() {
 	return 0;
+}
 
-   }
-   
-   // return (but do not delete) item from the front
-   double peek(){
+// is the buffer empty (size equals zero)?
+boolean isEmpty() {
+	return false;
+}
+
+// is the buffer full  (size equals capacity)?
+boolean isFull() {
+	return false;
+}
+
+// add item x to the end
+void enqueue(double x) {
+
+}
+
+// delete and return item from the front
+double dequeue() {
 	return 0;
+}
 
-   }
-	
-	
-	
+// return (but do not delete) item from the front
+double peek() {
+	return 0;
+}
 
 }
