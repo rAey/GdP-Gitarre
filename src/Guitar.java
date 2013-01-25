c2c3e064ae1b7aa65e085d3aeb70de5d9b58933b
Robert Fritz <rAey1@gmx.de>
2013-01-25 23:58:42 +0100

    first implementations

--- src/Guitar.java
+++ src/Guitar.java
@@ -15,21 +15,106 @@ public class Guitar {
 	Sample. By the superposition principle, you can get a sample of your guitar by adding the samples of all strings
 	**/
 	
+	GuitarString strE;
+	GuitarString strA;
+	GuitarString strD;
+	GuitarString strG;
+	GuitarString strB;
+	GuitarString stre;
+	
     Guitar(){
-    	
+    	strE = new GuitarString(82.41);
+    	strA = new GuitarString(110.00);
+    	strD = new GuitarString(146.83);
+    	strG = new GuitarString(196.00);
+    	strB = new GuitarString(246.94);
+    	stre = new GuitarString(329.63);
     }
+    
     void pluckString(int string){
+
+    	switch( string){
+    	case 1:
+    		stre.pluck();
+    		break;
+    		
+    	case 2:
+    		strB.pluck();
+    		break;
+    		
+    	case 3:
+    		strG.pluck();
+    		break;
+    		
+    	case 4:
+    		strD.pluck();
+    		break;
+    		
+    	case 5:
+    		strA.pluck();
+    		break;
+    	
+    	case 6:
+    		strE.pluck();
+    		break;
+    		
+    	default:
+    		break;
+    	}
+    		
     	// pluck the corresponding string
     }
+	
+    // mute every string
     void mute(){
-    	// mute every string
+    	strE.mute();
+    	strA.mute();
+    	strD.mute();
+    	strG.mute();
+    	strB.mute();
+    	stre.mute();
     }
+    
     void pressFretDown(int string, int fret){
-    	// press a fret on a string down
+    	switch( string){
+    	case 1:
+    		stre.pressFretDown(fret);
+    		break;
+    		
+    	case 2:
+    		strB.pressFretDown(fret);
+    		break;
+    		
+    	case 3:
+    		strG.pressFretDown(fret);
+    		break;
+    		
+    	case 4:
+    		strD.pressFretDown(fret);
+    		break;
+    		
+    	case 5:
+    		strA.pressFretDown(fret);
+    		break;
+    	
+    	case 6:
+    		strE.pressFretDown(fret);
+    		break;
+    		
+    	default:
+    		break;
+    	}
     }
+    
     void tic(){
-    	// advance the simulation one every string
+    	strE.tic();
+    	strA.tic();
+    	strD.tic();
+    	strG.tic();
+    	strB.tic();
+    	stre.tic();
     }
+    
     double sample(){
 		return 0;
 	  // compute a sample of the guitar output
