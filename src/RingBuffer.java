2a360925e3de5357e0d447cdfc5564f2b584e3c4
Dustin Fechner <dfe@hush.com>
2013-01-29 19:51:53 +0100

    Fix: RingBuffer readability

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -8,69 +8,71 @@ private double buffer[];
 private int first, last, size;
 
 // constructor to create an empty buffer, with given max capacity
-RingBuffer(int capacity) {
+RingBuffer(int capacity)
+{
 	buffer = new double[capacity];
 	first = last = size = 0;
 }
 
 // return number of items currently in the buffer
-int size() {
+int size()
+{
 	return size;
 }
 
 // is the buffer empty (size equals zero)?
-boolean isEmpty() {
+boolean isEmpty()
+{
 	if (size <= 0)
 		return true;
 	return false;
 }
 
 // is the buffer full  (size equals capacity)?
-boolean isFull() {
+boolean isFull()
+{
 	if (size >= buffer.length)
 		return true;
 	return false;
 }
 
 // add item x to the end
-void enqueue(double x) {
-	if (isFull()) {
+void enqueue(double x)
+{
+	if (isFull())
 		throw new BufferOverflowException();
-	}
 
 	buffer[last] = x;
-
 	size++;
 	if (++last >= buffer.length)
 		last = 0;
 }
 
 // delete and return item from the front
-double dequeue() {
-	if (isEmpty()) {
+double dequeue()
+{
+	if (isEmpty())
 		throw new BufferUnderflowException();
-	}
 
 	double x = buffer[first];
-
 	size--;
 	if (++first >= buffer.length)
 		first = 0;
-
 	return x;
 }
 
 // return (but do not delete) item from the front
-double peek() {
-	if (isEmpty()) {
+double peek()
+{
+	if (isEmpty())
 		throw new BufferUnderflowException();
-	}
 
 	return buffer[first];
 }
 
 // clear the buffer, so it can be filled with new elements
-void clear() {
+void clear()
+{
 	first = last = size = 0;
 }
 
